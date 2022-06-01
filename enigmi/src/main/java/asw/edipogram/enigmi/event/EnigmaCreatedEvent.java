cf15ae19ebe8a79eb608964b6ff1d54517588d50
paolo <tardioli.paolo@gmail.com>
2022-06-01 10:17:16 +0200

    Aggiunto evento EnigmaCreatedEvent

---
+++ enigmi/src/main/java/asw/edipogram/enigmi/event/EnigmaCreatedEvent.java
@@ -0,0 +1,19 @@
+package asw.edipogram.enigmi.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class EnigmaCreatedEvent implements DomainEvent {
+
+    private Long id;
+    private String autore;
+    private String tipo;
+    private String tipoSpecifico;
+    private String titolo;
+    private String[] testo;
+    private String[] soluzione;
+
+}
