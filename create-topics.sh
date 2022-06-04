fcdd432a46705be9c424a3b4016b8e5d9d804ce3
paolo <tardioli.paolo@gmail.com>
2022-06-04 14:00:16 +0200

    Aggiornamento nome topics

--- create-topics.sh
+++ create-topics.sh
@@ -4,6 +4,9 @@ echo Creating some Kafka topics...
 
 KAFKA_DOCKER=$(docker ps | grep kafka | grep -v zookeeper | awk '{print $1}')
 
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic TopicA --replication-factor 1 --partitions 1
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic TopicB --replication-factor 1 --partitions 1
+docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic TopicNewEnigmi --replication-factor 1 --partitions 1
+docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic TopicNewConnessioni --replication-factor 1 --partitions 1
+
+
+
 
