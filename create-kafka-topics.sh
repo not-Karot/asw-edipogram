30f6168523509ea5dd00d4b01ba5438ddf623b89
Tibbo93 <alessandrotibaldi93@gmail.com>
2022-06-07 02:34:31 +0200

    Aggiornato script create-kafka-topics

--- create-kafka-topics.sh
+++ create-kafka-topics.sh
@@ -4,6 +4,6 @@ echo Creating some Kafka topics...
 
 KAFKA_DOCKER=$(docker ps | grep kafka | grep -v zookeeper | awk '{print $1}')
 
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic asw.edipogram.enigmi --replication-factor 1 --partitions 1
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic asw.edipogram.connessioni --replication-factor 1 --partitions 1
+docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic enigma-event-channel --replication-factor 1 --partitions 1
+docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic connessioni-event-channel --replication-factor 1 --partitions 1
 
